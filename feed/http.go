cd5865d20aaf976b5f9738d7322a1a972b580cc7
Andrew Wansley <andrew.wansley@gmail.com>
2016-08-31 09:34:10 -0700

    Experimental sandstorm API hax

--- feed/http.go
+++ feed/http.go
@@ -2,6 +2,8 @@ package feed
 
 import (
 	"net/http"
+	"net/url"
+	"strings"
 	"sync"
 )
 
@@ -23,9 +25,29 @@ func Initialize(getter Getter) {
 
 // Get executes an HTTP get
 // Calling Get before Initialize is an error
-func Get(url string) (*http.Response, error) {
+func Get(requestedURL string) (*http.Response, error) {
 	if instance == nil {
 		panic("Calling Get before initialization")
 	}
-	return instance.Get(url)
+
+	// Handle special sandstorm URL format
+	splot := strings.Split(requestedURL, "#")
+	if len(splot) == 2 {
+		u, err := url.Parse(splot[0])
+		if err != nil {
+			return nil, err
+		}
+		u.User = url.UserPassword("anonymous-user", splot[1])
+		// HACK: if we know the node is a sandstorm node,
+		// we have to drop "sync" from the path as sandstorm will add it automatically
+		// as a defense mechanism
+		p := u.EscapedPath()
+		if strings.HasPrefix(p, "/sync") {
+			p = p[len("/sync"):]
+		}
+		u.Path = p
+
+		requestedURL = u.String()
+	}
+	return instance.Get(requestedURL)
 }
